29b53c3d96bd20b948c69fd929736c5e777e80c2
James <jmborden@gmail.com>
2017-05-05 16:41:50 -0500

    refactored xhr.cljs to use $ macro

--- src/cljs/greedy_pigeon/xhr.cljs
+++ src/cljs/greedy_pigeon/xhr.cljs
@@ -1,20 +1,22 @@
 (ns greedy-pigeon.xhr
+  (:require-macros [reagent.interop :refer [$ $!]])
   (:require [goog.net.XhrIo]))
+
+(def XhrIo goog.net.XhrIo)
 
 (defn send-xhr
   "Send a xhr to url using callback and HTTP method."
   [url callback method & [data headers timeout]]
-  (.send goog.net.XhrIo url callback method data headers timeout))
+  ($ XhrIo send url callback method data headers timeout))
 
 (defn xhrio-wrapper
   "A callback for processing the xhrio response event. If
   response.target.isSuccess() is true, call f on the json response"
   [f response]
-  (let [target (.-target response)]
-    (if (.isSuccess target)
-      (f (.getResponseJson target))
-      (.log js/console
-            (str "xhrio-wrapper error:" (aget target "lastError_"))))))
+  (let [target ($ response :target)]
+    (if ($ target isSuccess)
+      (f ($ target getResponseJson))
+      ($ js/console log (str "xhrio-wrapper error:" ($ target :lastError_))))))
 
 (defn retrieve-url
   "Retrieve and process json response with f from url using HTTP method and json
